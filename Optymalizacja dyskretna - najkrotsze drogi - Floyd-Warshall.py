3723faac4fad42f27c2c3fbab309fe2f2b41955e
shrimy <prybka2@gmail.com>
2024-04-14 19:26:17 +0200

    Dodanie komentarzy wyjaśniających działanie poszczególnych części kodu.

--- Optymalizacja dyskretna - najkrotsze drogi - Floyd-Warshall.py
+++ Optymalizacja dyskretna - najkrotsze drogi - Floyd-Warshall.py
@@ -3,104 +3,117 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-def generate_connected_directed_graph(num_nodes, num_edges):
-    if num_edges < num_nodes:
+def generuj_graf(l_wierzcholkow, l_krawedzi):
+    # Funkcja generująca losowy graf skierowany z wagami na krawędziach
+    if l_krawedzi < l_wierzcholkow:
         raise ValueError("Liczba krawędzi musi być większa lub równa liczbie wierzchołków.")
 
     G = nx.DiGraph()
-    nodes = range(1, num_nodes + 1)
+    nodes = range(1, l_wierzcholkow + 1)
     G.add_nodes_from(nodes)
 
-    for i in range(1, num_nodes):
-        weight = random.randint(-5, 10)
-        G.add_edge(i, i + 1, weight=weight)
-    weight = random.randint(-5, 10)
-    G.add_edge(num_nodes, 1, weight=weight)
+    # Dodanie losowych krawędzi między kolejnymi wierzchołkami
+    for i in range(1, l_wierzcholkow):
+        waga = random.randint(-5, 10)  # Zakres losowania wag krawędzi
+        G.add_edge(i, i + 1, weight=waga)
+    waga = random.randint(-5, 10)  # Zakres losowania wag krawędzi
+    G.add_edge(l_wierzcholkow, 1, weight=waga)
 
-    max_possible_edges = num_nodes * (num_nodes - 1)
-    num_edges = min(num_edges, max_possible_edges)
+    # Ustalenie maksymalnej liczby krawędzi i wygenerowanie losowych krawędzi
+    maks_krawedzi = l_wierzcholkow * (l_wierzcholkow - 1)
+    l_krawedzi = min(l_krawedzi, maks_krawedzi)
 
-    while G.number_of_edges() < num_edges:
-        u = random.randint(1, num_nodes)
-        v = random.randint(1, num_nodes)
+    while G.number_of_edges() < l_krawedzi:
+        u = random.randint(1, l_wierzcholkow)
+        v = random.randint(1, l_wierzcholkow)
         if u != v and not G.has_edge(u, v):
-            weight = random.randint(-5, 10)
-            G.add_edge(u, v, weight=weight)
+            waga = random.randint(-5, 10)  # Zakres losowania wag krawędzi
+            G.add_edge(u, v, weight=waga)
             # Jeśli krawędź w drugą stronę już istnieje, ustaw jej wagę na identyczną
             if G.has_edge(v, u):
                 G[u][v]['weight'] = G[v][u]['weight']
     return G
 
-def generate_weight_matrix(graph):
-    num_nodes = len(graph.nodes())
-    weight_matrix = np.zeros((num_nodes, num_nodes))
+def generuj_macierz_wag(graf):
+    # Generowanie macierzy wag na podstawie danego grafu
+    l_wierzcholkow = len(graf.nodes())
+    macierz_wag = np.zeros((l_wierzcholkow, l_wierzcholkow))
 
-    for u, v, weight in graph.edges(data='weight'):
-        weight_matrix[u-1][v-1] = weight
+    for u, v, weight in graf.edges(data='weight'):
+        macierz_wag[u-1][v-1] = weight
 
-    return weight_matrix
+    return macierz_wag
 
 # Funkcja do rysowania grafu za pomocą NetworkX
-def draw_graph(graph):
+def rysuj_graf(graf):
+    # Rysowanie grafu wraz z wagami krawędzi
     plt.figure(figsize=(8, 6))
-    pos = nx.spring_layout(graph)
-    edge_labels = nx.get_edge_attributes(graph, 'weight')  # Pobieramy wagi krawędzi
-    nx.draw(graph, pos, with_labels=True, node_size=500, node_color='skyblue', font_size=12, arrowsize=10)
-    nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels)  # Dodajemy etykiety do krawędzi
+    pos = nx.spring_layout(graf)
+    edge_labels = nx.get_edge_attributes(graf, 'weight')  # Pobranie wag krawędzi
+    nx.draw(graf, pos, with_labels=True, node_size=500, node_color='skyblue', font_size=12, arrowsize=10)
+    nx.draw_networkx_edge_labels(graf, pos, edge_labels=edge_labels)  # Dodanie etykiet do krawędzi
     plt.title("Wygenerowany graf")
     plt.show()
 
 # Funkcja do wypisywania macierzy incydencji
-def print_incidence_matrix(graph):
-    incidence_matrix = nx.incidence_matrix(graph, oriented=True).toarray()
+def wypisz_macierz_incydencji(graf):
+    # Wypisanie macierzy incydencji dla danego grafu
+    macierz_incydencji = nx.incidence_matrix(graf, oriented=True).toarray()
     print("Macierz incydencji:")
-    print(incidence_matrix)
+    print(macierz_incydencji)
+
+def floyd_warshall(graf):
+    # Algorytm Floyd-Warshall dla wyznaczania najkrótszych ścieżek między wszystkimi parami wierzchołków
+    l_wierzcholkow = len(graf.nodes())
+    odleglosc = np.zeros((l_wierzcholkow, l_wierzcholkow))
+
+    # Inicjalizacja macierzy odległości
+    for i in range(l_wierzcholkow):
+        for j in range(l_wierzcholkow):
+            if i == j:
+                odleglosc[i][j] = 0
+            elif graf.has_edge(i + 1, j + 1):
+                odleglosc[i][j] = graf[i + 1][j + 1]['weight']
+            else:
+                odleglosc[i][j] = float('inf')
+
+    # Algorytm Floyda-Warshalla
+    for k in range(l_wierzcholkow):
+        print("Macierz odległości po iteracji", k+1, ":")
+        print(odleglosc)
+        print()  # Dodaj pusty wiersz dla czytelności
+        for i in range(l_wierzcholkow):
+            for j in range(l_wierzcholkow):
+                if odleglosc[i][k] + odleglosc[k][j] < odleglosc[i][j]:
+                    odleglosc[i][j] = odleglosc[i][k] + odleglosc[k][j]
+
+    return odleglosc
 
 # Przykładowe użycie funkcji
 while True:
     try:
-        num_nodes = int(input("Podaj liczbę wierzchołków: "))
-        num_edges = int(input("Podaj liczbę krawędzi: "))
-        graph = generate_connected_directed_graph(num_nodes, num_edges)
+        l_wierzcholkow = int(input("Podaj liczbę wierzchołków: "))
+        l_krawedzi = int(input("Podaj liczbę krawędzi: "))
+        graf = generuj_graf(l_wierzcholkow, l_krawedzi)
         break
     except ValueError as e:
         print(e)
 
-print_incidence_matrix(graph)
+# Wypisanie macierzy incydencji dla wygenerowanego grafu
+wypisz_macierz_incydencji(graf)
+
 # Generuj i wyświetl macierz wag
-weight_matrix = generate_weight_matrix(graph)
+macierz_wag = generuj_macierz_wag(graf)
 print("Macierz wag:")
-print(weight_matrix)
-draw_graph(graph)
+print(macierz_wag)
 
-def floyd_warshall(graph):
-    num_nodes = len(graph.nodes())
-    dist = np.zeros((num_nodes, num_nodes))
+# Rysowanie wygenerowanego grafu
+rysuj_graf(graf)
 
-    # Inicjalizacja macierzy odległości
-    for i in range(num_nodes):
-        for j in range(num_nodes):
-            if i == j:
-                dist[i][j] = 0
-            elif graph.has_edge(i+1, j+1):
-                dist[i][j] = graph[i+1][j+1]['weight']
-            else:
-                dist[i][j] = float('inf')
-
-    # Algorytm Floyda-Warshalla
-    for k in range(num_nodes):
-        print("Macierz odległości po iteracji", k+1, ":")
-        print(dist)
-        print()  # Dodaj pusty wiersz dla czytelności
-        for i in range(num_nodes):
-            for j in range(num_nodes):
-                if dist[i][k] + dist[k][j] < dist[i][j]:
-                    dist[i][j] = dist[i][k] + dist[k][j]
-
-    return dist
-
-# Przykładowe użycie
-distances = floyd_warshall(graph)
+# Wykonanie algorytmu Floyd-Warshalla i wyświetlenie macierzy odległości
+odleglosc = floyd_warshall(graf)
 print("Macierz odległości końcowej:")
-print(distances)
-draw_graph(graph)
+print(odleglosc)
+
+# Ponowne rysowanie wygenerowanego grafu
+rysuj_graf(graf)
